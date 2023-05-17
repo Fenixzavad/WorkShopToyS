4f6ccc8cb57f07382aa1b3ee9ac70eadd26325ee
fenixzava <fenixzava@gmail.com>
2023-05-17 23:23:16 +0300

    create program

---
+++ Model/Infrastructure/Interfaces/EntityFactory.java
@@ -0,0 +1,8 @@
+package Model.Infrastructure.Interfaces;
+
+import Model.Entities.AbstractClasses.Entity;
+
+public interface EntityFactory<T extends Entity> 
+{
+    public T createEntity(String data);
+}
